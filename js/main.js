8f3aaa324c84d9bbc59c1cb329afa63e916fd5a3
t105590028 <t105590028@ntut.org.tw>
2018-06-24 20:18:12 +0800

    1

---
+++ js/main.js
@@ -0,0 +1,85 @@
+var y=0;
+
+var rownav=0;
+var wdth=$(window).width();
+$(window).resize(function() {
+    wdth=$(window).width()
+});
+if ($(this).scrollTop() >= 473) {        // If page is scrolled more than 50px
+    $('#return-to-top').fadeIn(200);    // Fade in the arrow
+} 
+if ($(this).scrollTop() > 0) {
+    $('.row_navbar').addClass('row_navbar_1');
+}
+if($(this).scrollTop()==0&&wdth<=1250){
+    $('.row_navbar').addClass('row_navbar_1');
+}
+if(wdth>1250){
+    $('.row_nav').removeClass('row_nav_1');
+    rownav=0;
+}
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 473) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else if($(this).scrollTop()>0&&y==0&&wdth>1250){
+       $('.row_navbar').addClass('row_navbar_1');
+       $('body,html').animate({
+            scrollTop : 947                       
+        }, 750);
+    }
+    else if($(this).scrollTop() ==0&&wdth>1250){
+        $('.row_navbar').removeClass('row_navbar_1');
+    }
+    else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+    y=$(this).scrollTop();
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 750);
+});
+$('#rownav').click(function() {      // When arrow is clicked
+    $('.row_nav').addClass('row_nav_1');
+});
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+    $(window).resize(function() {
+        if (wdth <1233&&$(this).scrollTop()==0) {        
+           $('.row_navbar').addClass('row_navbar_1');
+        }
+        else if(wdth>=1233&&$(this).scrollTop()==0){
+            $('.row_navbar').removeClass('row_navbar_1');
+        }
+        else if(wdth>1250){
+            $('.row_nav').removeClass('row_nav_1');
+            rownav=0;
+        }
+    });
+});
+
+$('.handle').on('click',function(){
+    if(rownav==0){
+        $('.row_nav').addClass('row_nav_1');
+        rownav=1;
+    }
+    else{
+        $('.row_nav').removeClass('row_nav_1');
+        rownav=0;
+    }
+});
+$('.handle1').on('click',function(){
+    if(rownav==0){
+        $('.row_nav').addClass('row_nav_2');
+        rownav=1;
+    }
+    else{
+        $('.row_nav').removeClass('row_nav_2');
+        rownav=0;
+    }
+});
+$('.navbar_row').click(function(){
+    $('.row_nav').removeClass('row_nav_1');
+        rownav=0;
+});
